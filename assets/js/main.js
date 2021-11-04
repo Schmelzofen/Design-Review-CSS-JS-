cacd7885aa2c197b6cfea8c9d08ca4f865deb5c5
Schmelzofen <sertac.oezdemir@gmail.com>
2021-11-04 09:42:51 +0100

    Forms JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 let subscribe = document.getElementById("subscribe")
 let subscribed = document.getElementById("subscribed")
 
+let email = document.getElementById("email")
+let message = document.getElementById("message")
+let name = document.getElementById("name")
+
 function subscribeForm(){
     subscribe.classList.remove("hide")
     subscribe.classList.add("show")
@@ -12,6 +16,9 @@ function sendMessage(){
     subscribe.classList.add("hide")
     subscribed.classList.remove("hide")
     subscribed.classList.remove("show")
+    console.log(name.value)
+    console.log(email.value)
+    console.log(message.value)
 }
 
 function closeWindow(){
